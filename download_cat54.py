a83228f30056c04fffb873dd9a0da6c5f78b045d
chase001@qq.com <chase001@qq.com>
2019-03-26 23:58:45 +0800

    20190326

---
+++ download_cat54.py
@@ -0,0 +1,15 @@
+# import urllib.request
+#
+# response = urllib.request.urlopen("http://placekitten.com/g/200/300")
+# cat_img = response.read()
+#
+# with open('cat_200_300.jpg', 'wb') as f:
+#     f.write(cat_img)
+
+import urllib.request
+
+response = urllib.request.urlopen("http://placekitten.com/g/200/300")
+cat_img = response.read()
+
+with open('cat_200_3002.jpg','ab') as f:
+    f.write(cat_img)
